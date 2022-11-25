6f109dd532f4e435b961900f4360693b034068f4
Devedse <davydavidse@gmail.com>
2022-11-25 15:11:24 +0100

    Removed adventure game

--- DeveFileFlows.Client/Program.cs
+++ DeveFileFlows.Client/Program.cs
@@ -1,64 +1,55 @@
-using AdventureGrainInterfaces;
 using DeveFileFlows.GrainInterfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-using var host = Host.CreateDefaultBuilder(args)
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using var host = Host.CreateDefaultBuilder(args)
     .UseOrleansClient(clientBuilder =>
         clientBuilder.UseLocalhostClustering())
     .Build();
 
-await host.StartAsync();
+        await host.StartAsync();
 
-var client = host.Services.GetRequiredService<IClusterClient>();
+        var client = host.Services.GetRequiredService<IClusterClient>();
 
-var fileFlow1 = client.GetGrain<IFileFlowGrain>(0);
+        var fileFlow1 = client.GetGrain<IFileFlowGrain>(0);
 
-var theconfig = await fileFlow1.GetFileFlowConfig();
-Console.WriteLine(theconfig.Name);
+        var theconfig = await fileFlow1.GetFileFlowConfig();
+        Console.WriteLine(theconfig.Name);
 
-var thenewconfig = new FileFlowConfig()
-{
-    Name = "CompressImage",
-    Description = "Hallo dit is een onschrijving die het graan beschrijft"
-};
-await fileFlow1.SetFileFlowConfig(thenewconfig);
-await fileFlow1.SetSteps(new List<FileFlowStep>()
+        var thenewconfig = new FileFlowConfig()
+        {
+            Name = "CompressImage",
+            Description = "Hallo dit is een onschrijving die het graan beschrijft"
+        };
+        await fileFlow1.SetFileFlowConfig(thenewconfig);
+        await fileFlow1.SetSteps(new List<FileFlowStep>()
+        {
+            new FileFlowStep(
+                "C:\\Program Files\\FileOptimizer\\Plugins64\\jpegoptim.exe",
+                $"-o --all-progressive \""+"%INPUTFILETOKEN%"+"\"")
+        });
+
+
+        var flowRunnerGrain = client.GetGrain<IFlowRunnerGrain>(0);
+        await flowRunnerGrain.RunFlow(0, "FileName");
+        Console.WriteLine("Flow ran :)");
+
+        var result = "";
+        try
+        {
+            while (result is not "exit")
             {
-                new FileFlowStep(
-                    "C:\\Program Files\\FileOptimizer\\Plugins64\\jpegoptim.exe",
-                    $"-o --all-progressive \""+"%INPUTFILETOKEN%"+"\"")
-            });
-
-
-var flowRunnerGrain = client.GetGrain<IFlowRunnerGrain>(0);
-await flowRunnerGrain.RunFlow(0, "FileName");
-
-Console.WriteLine();
-Console.WriteLine("What's your name?");
-var name = Console.ReadLine()!;
-var player = client.GetGrain<IPlayerGrain>(Guid.NewGuid());
-await player.SetName(name);
-
-var room1 = client.GetGrain<IRoomGrain>(0);
-await player.SetRoomGrain(room1);
-
-Console.WriteLine(await player.Play("look"));
-
-var result = "Start";
-try
-{
-    while (result is not "")
-    {
-        var command = Console.ReadLine()!;
-
-        result = await player.Play(command);
-        Console.WriteLine(result);
+                result = Console.ReadLine()!;
+            }
+            Console.WriteLine("Exit, byee");
+        }
+        finally
+        {
+            await host.StopAsync();
+        }
     }
-}
-finally
-{
-    await player.Die();
-    Console.WriteLine("Game over!");
-    await host.StopAsync();
 }
--- DeveFileFlows.Server/Program.cs
+++ DeveFileFlows.Server/Program.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using AdventureSetup;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -7,27 +6,6 @@ internal class Program
 {
     private static async Task<int> Main(string[] args)
     {
-        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
-        var mapFileName = Path.Combine(path, "AdventureMap.json");
-
-        switch (args.Length)
-        {
-            default:
-                Console.WriteLine("*** Invalid command line arguments.");
-                return -1;
-            case 0:
-                break;
-            case 1:
-                mapFileName = args[0];
-                break;
-        }
-
-        if (!File.Exists(mapFileName))
-        {
-            Console.WriteLine("*** File not found: {0}", mapFileName);
-            return -2;
-        }
-
         // Configure the host
         using var host = Host.CreateDefaultBuilder(args)
             .UseOrleans(siloBuilder =>
@@ -45,13 +23,9 @@ internal class Program
         // Start the host
         await host.StartAsync();
 
-        Console.WriteLine("Map file name is '{0}'.", mapFileName);
-        Console.WriteLine("Setting up Adventure, please wait ...");
-
         // Initialize the game world
         var client = host.Services.GetRequiredService<IGrainFactory>();
-        var adventure = new AdventureGame(client);
-        await adventure.Configure(mapFileName);
+
 
         Console.WriteLine("Setup completed.");
         Console.WriteLine("Now you can launch the client.");
